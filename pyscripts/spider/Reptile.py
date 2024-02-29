62ed3d6289dbb02a6c125e752985ae291afdaa86
lzk <luannanxian@gmail.com>
2024-02-29 20:24:32 +0800

    适配chrome

--- pyscripts/spider/Reptile.py
+++ pyscripts/spider/Reptile.py
@@ -14,19 +14,25 @@ import re
 import shutil
 import pandas as pd
 
+
 def print_log(msg: str, log_path='log.txt'):
     localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     with open(log_path, 'a', encoding='utf-8') as f:
         f.write(localtime + ' ' + msg + '\n')
 
+
+sleep_time = 3
+
 class Reptile:
-    def dump_and_delay(self, driver: webdriver.Edge, url: str):
+    def dump_and_delay(self, driver: webdriver.chrome, url: str):
         print_log('跳转网页{0}'.format(url))
         driver.get(url)
-        time.sleep(31)  # 防止反爬虫检测
+        windows = driver.window_handles
+        driver.switch_to.window(windows[-1])
+        time.sleep(3)  # 防止反爬虫检测
 
     # 先进入对应的联赛，然后爬出所有的赛季网址，再遍历所有网址拿到联赛数据
-    def go_to_match(self, driver: webdriver.Edge, match_name: str, out_season_url_list: list):
+    def go_to_match(self, driver: webdriver.chrome, match_name: str, out_season_url_list: list):
         all_match = driver.find_element(By.ID, 'select_notcups')
         match = all_match.find_element(By.XPATH, '//option[contains(text(), "{0}")]'.format(match_name))
         match_value = match.get_attribute('value')
@@ -41,16 +47,16 @@ class Reptile:
             sub_url = s.get_attribute('href')
             out_season_url_list.append(sub_url)
 
-    def get_season_data(self, driver: webdriver.Edge, url: str, sche: Schedule):
+    def get_season_data(self, driver: webdriver.chrome, url: str, sche: Schedule):
         self.dump_and_delay(driver, url)
         cur_round_index = 0
         while cur_round_index < sche.cur_round_index:
             rounds = driver.find_elements(By.XPATH, '//span[@class="lcol_hd_lun"]/a')
             rounds[0].click()
             cur_round_index += 1
-            time.sleep(4)
+            time.sleep(3)
 
-        self.get_round_data(driver, url, cur_round_index) # 回来后还是 cur_round_index
+        self.get_round_data(driver, url, cur_round_index)  # 回来后还是 cur_round_index
 
         # 表格有多轮
         next_round = driver.find_elements(By.XPATH, '//span[@class="lcol_hd_lun"]')
@@ -67,7 +73,7 @@ class Reptile:
 
             next_round = driver.find_elements(By.XPATH, '//span[@class="lcol_hd_lun"]')
 
-    def get_round_data(self, driver: webdriver.Edge, url: str, round_index: int):
+    def get_round_data(self, driver: webdriver.chrome, url: str, round_index: int):
         all_match = driver.find_elements(By.XPATH, '//tbody[@class="jTrInterval"]/tr')
         i = 0
         count = len(all_match)
@@ -83,7 +89,8 @@ class Reptile:
 
             # 还没出比赛结果 或 没有平均欧指
             # 如果已经保存了，就不再保存
-            if ('vs' in m_info[2].text.lower()) or (len(m_info[5].text.strip()) <= 0) or os.path.exists(match_status.get_save_path()):
+            if ('vs' in m_info[2].text.lower()) or (len(m_info[5].text.strip()) <= 0) or os.path.exists(
+                    match_status.get_save_path()):
                 continue
 
             analyze_url = m_info[6].find_element(By.XPATH, './/a').get_attribute('href')
@@ -99,10 +106,10 @@ class Reptile:
                 rounds[0].click()
                 time.sleep(4)
 
-
-    def get_match_data(self, driver: webdriver.Edge, match_status: MatchStatus):
+    def get_match_data(self, driver: webdriver.chrome, match_status: MatchStatus):
         score_str = driver.find_element(By.XPATH,
                                         '//div[@class="odds_hd_center"]/p[@class="odds_hd_bf"]/strong').text.strip()
+
         score = score_str.split(':')
         if 'vs' in score_str.lower():
             return
@@ -114,8 +121,8 @@ class Reptile:
         olddownpl.click()  # 下载欧赔
         time.sleep(4)
         print_log('finish down ouzhi step:{0} {1} vs {2}'.format(match_status.timestamp, match_status.home_team,
-                                                            match_status.away_team))
-        try: # 可能 excel 解析错误
+                                                                 match_status.away_team))
+        try:  # 可能 excel 解析错误
             self.process_oupei(match_status)
         except:
             error_msg = traceback.format_exc()
@@ -126,14 +133,14 @@ class Reptile:
         downpl.click()  # 下载亚盘
         time.sleep(4)
         print_log('finish down yapan step:{0} {1} vs {2}'.format(match_status.timestamp, match_status.home_team,
-                                                            match_status.away_team))
+                                                                 match_status.away_team))
         self.process_yapan(match_status)
 
         match_status.save_data(match_status.get_save_path())
         pass
 
-    def match_and_move_download_file(self, save_dic_path: str, pattern: str, prefix:str = '', try_times: int = 10):
-        download_path = 'C:\\Users\\90571\\Downloads'
+    def match_and_move_download_file(self, save_dic_path: str, pattern: str, prefix: str = '', try_times: int = 10):
+        download_path = 'competition/download'
         tf = None
         cnt = 0
         while (not tf) and cnt < try_times:
@@ -149,23 +156,25 @@ class Reptile:
         if not tf:
             return None
 
-        source_path = os.path.join(download_path, tf) # 这里报错就是没有下载好
+        source_path = os.path.join(download_path, tf)  # 这里报错就是没有下载好
         target_path = os.path.join(save_dic_path, prefix + tf)
         if not os.path.exists(save_dic_path):
             os.makedirs(save_dic_path)
         shutil.move(source_path, target_path)
         return target_path
 
-    def process_oupei(self, match_status: MatchStatus, save_dic_path: str = '{0}/competition/europe'.format(os.getcwd())):
+    def process_oupei(self, match_status: MatchStatus,
+                      save_dic_path: str = '{0}/competition/europe'.format(os.getcwd())):
         xml_path = self.match_and_move_download_file(save_dic_path, '欧洲数据', match_status.timestamp.split(' ')[0])
 
         if not xml_path:
-            print_log('process oupei error:'.format(match_status.timestamp, match_status.home_team, match_status.away_team))
+            print_log(
+                'process oupei error:'.format(match_status.timestamp, match_status.home_team, match_status.away_team))
             return
 
         df_sheet_list = pd.ExcelFile(xml_path)
         sheet_name = df_sheet_list.sheet_names[1]
-        oupei_xls = pd.read_excel(xml_path, sheet_name=sheet_name, usecols="A,L:U", skiprows=lambda x:x in range(0, 3))
+        oupei_xls = pd.read_excel(xml_path, sheet_name=sheet_name, usecols="A,L:U", skiprows=lambda x: x in range(0, 3))
         avg = [format(x, '.5f') for x in oupei_xls.values[0][1:]]
         discrete = [str(x) for x in oupei_xls.values[1][1:4]]
         match_status.set_oupei_input(avg, discrete)
@@ -175,30 +184,81 @@ class Reptile:
             match_status.add_ouzhi(o)
         pass
 
-    def process_yapan(self, match_status: MatchStatus, save_dic_path: str = '{0}/competition/asia_andicap'.format(os.getcwd())):
+    def process_yapan(self, match_status: MatchStatus,
+                      save_dic_path: str = '{0}/competition/asia_andicap'.format(os.getcwd())):
         xml_path = self.match_and_move_download_file(save_dic_path, '亚盘', match_status.timestamp.split(' ')[0])
 
         if not xml_path:
-            print_log('process yapan error:'.format(match_status.timestamp, match_status.home_team, match_status.away_team))
+            print_log(
+                'process yapan error:{},{},{}'.format(match_status.timestamp, match_status.home_team, match_status.away_team))
             return
 
-        yapan_xls = pd.read_excel(xml_path, usecols="A, F:H", skiprows=lambda x:x in range(0, 4))
+        yapan_xls = pd.read_excel(xml_path, usecols="A, F:H", skiprows=lambda x: x in range(0, 4))
         for v in yapan_xls.values:
             yapan = [x for x in v[1:]]
             y = YaPan(v[0], yapan)
             match_status.add_yapan(y)
         pass
 
-    def begin(self, ):
+    def begin(self):
         # 浏览器设置
-        options = webdriver.EdgeOptions()
+        # options = webdriver.EdgeOptions()
+
+        options = webdriver.ChromeOptions()
+
+        # 添加UA
+        options.add_argument(
+            'user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36"')
+
+        # 指定浏览器分辨率
+        options.add_argument('window-size=1920x3000')
+
+        # 谷歌文档提到需要加上这个属性来规避bug
+        options.add_argument('--disable-gpu')
+
+        # 隐藏滚动条, 应对一些特殊页面
+        options.add_argument('--hide-scrollbars')
+
+        # 不加载图片, 提升速度
+        options.add_argument('blink-settings=imagesEnabled=false')
+
+        # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+        options.add_argument('--headless')
+
+        # 以最高权限运行
+        options.add_argument('--no-sandbox')
+
+        # 手动指定使用的浏览器位置
+        # options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+
+        # 添加crx插件
+        # options.add_extension('d:\crx\AdBlock_v2.17.crx')
+
+        # 禁用JavaScript
+        options.add_argument("--disable-javascript")
+
+        # 设置开发者模式启动，该模式下webdriver属性为正常值
+        options.add_experimental_option('excludeSwitches', ['enable-automation'])
+
+        # 禁用浏览器弹窗
+        prefs = {
+            'profile.default_content_setting_values': {
+                'notifications': 2
+            }
+        }
+        options.add_experimental_option('prefs', prefs)
+
+
         # options.add_argument('headless') # 无界面
-        service = webdriver.EdgeService(executable_path='msedgedriver.exe')
+
+        # service = webdriver.EdgeService(executable_path='msedgedriver.exe')
 
         # 开始启动浏览器
         print_log('begin')
-        driver = webdriver.Edge(options=options, service=service)
-        driver.implicitly_wait(100)  # 隐式等待
+        driver = webdriver.Chrome(options=options)
+        # driver = webdriver.Edge(options=options, service=service)
+
+        driver.implicitly_wait(10)  # 隐式等待
         driver.get('https://liansai.500.com/zuqiu-194/')
 
         five_match = ['英超', '西甲', '意甲', '德甲', '法甲']
@@ -217,7 +277,7 @@ class Reptile:
         while i < len(five_match):
             if len(season_url_list) <= 0:
                 self.go_to_match(driver, five_match[i], season_url_list)
-                j = 2 # 2 开始是因为 2002 赛季开始才有欧赔指数
+                j = 2  # 2 开始是因为 2002 赛季开始才有欧赔指数
 
             while j < len(season_url_list):
                 # 保存记录
@@ -237,16 +297,16 @@ class Reptile:
     def test(self):
         # 浏览器设置
         options = webdriver.EdgeOptions()
-        options.add_argument('headless') # 无界面
-        service = webdriver.EdgeService(executable_path='msedgedriver.exe')
+        options.add_argument('headless')  # 无界面
+        # service = webdriver.EdgeService(executable_path='msedgedriver.exe')
 
         # 开始启动浏览器
         print_log('begin')
-        driver = webdriver.Edge(options=options, service=service)
-        driver.implicitly_wait(2)  # 隐式等待
-        driver.get('https://liansai.500.com/zuqiu-6858/')
-
-        self.get_season_data(driver, 'https://liansai.500.com/zuqiu-6858/', Schedule())
+        # driver = webdriver.Edge(options=options, service=service)
+        # driver.implicitly_wait(2)  # 隐式等待
+        # driver.get('https://liansai.500.com/zuqiu-6858/')
+        #
+        # self.get_season_data(driver, 'https://liansai.500.com/zuqiu-6858/', Schedule())
 
         while True:
             pass
